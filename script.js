258b879b3140cef5abd57352c7765ac2d60c81e2
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-10 21:13:41 +0100

    apply color function done

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ const addition = document.getElementById("add");
 
 addition.addEventListener("click", (e) => {
   counter.innerHTML++;
+  applyColor();
 });
 
 sub.addEventListener("click", () => {
   counter.innerHTML--;
+  applyColor();
 });
 
 reset.addEventListener("click", () => {
   const resetDone = 0;
   counter.innerHTML = resetDone;
 });
+
+const applyColor = () => {
+  if (counter.innerHTML > 0) {
+    counter.style.color = "green";
+  } else {
+    counter.style.color = "red";
+  }
+};
