ecdd20efafc835c597c46b2f44366b184be06adc
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-10 21:02:22 +0100

    addition js done

---
+++ script.js
@@ -0,0 +1,12 @@
+const counter = document.getElementById("value");
+const sub = document.getElementById("sub");
+const reset = document.getElementById("res");
+const addition = document.getElementById("add");
+
+addition.addEventListener("click", (e) => {
+  counter.innerHTML++;
+});
+
+sub.addEventListener("click", () => {
+  counter.innerHTML--;
+});
